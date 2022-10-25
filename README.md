38147333b31a6a0f5b290a30979f62e3d2fd7326
smcrowley8 <smcrowley8@gmail.com>
2022-10-25 18:20:10 -0400

    add axis labels and plot title to the form. cause plot to update properly

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Set of utilities for interactive data visualization
 
 ## Volcano Plot
 Replace the example ***./volcano_plot/html_data/report.tsv*** with your data. The following columns are used: **GeneId**, **log2FoldChange**, **padj**.
+
+## Development
+To view the html report and test it's interactivitiy, the html must be served.
+Run a python simple http server from within the html_data folder
+```bash
+cd ./volcano_plot/html_data
+# python 3
+python -m http.server 8000
+```
--- volcano_plot/html_data/index.js
+++ volcano_plot/html_data/index.js
@@ -1,11 +1,11 @@
 function volcanoPlot() {
     var width = 960,
         height = 500,
-        margin = {top: 20, right: 20, bottom: 40, left: 50},
+        margin = { top: 20, right: 20, bottom: 40, left: 50 },
         xColumn, // name of the variable to be plotted on the axis
         yColumn,
-        xAxisLabel, // label for the axis
-        yAxisLabel,
+        xAxisLabel = d3.select("#plotxaxis").property("value"),
+        yAxisLabel = d3.select("#plotyaxis").property("value"),
         xAxisLabelOffset, // offset for the label of the axis
         yAxisLabelOffset,
         xTicks, // number of ticks on the axis
@@ -15,55 +15,64 @@ function volcanoPlot() {
         foldChangeThreshold = parseFloat(d3.select("#foldchange").property("value")), // fold change level to colour by
         colorRange, // colour range to use in the plot
         xScale = d3.scaleLinear(), // the values for the axes will be continuous
-        yScale = d3.scaleLog();
+        yScale = d3.scaleLog(),
+        plotTitle = d3.select('#plottitle').property("value");
 
 
 
-    function chart(selection){
+    function chart(selection) {
         var innerWidth = width - margin.left - margin.right, // set the size of the chart within its container
             innerHeight = height - margin.top - margin.bottom;
 
-        selection.each(function(data) {
+        selection.each(function (data) {
 
             // set up the scaling for the axes based on the inner width/height of the chart and also the range
             // of value for the x and y axis variables. This range is defined by their min and max values as
             // calculated by d3.extent()
             xScale.range([0, innerWidth])
-                .domain(d3.extent(data, function(d) { return d[xColumn]; }))
+                .domain(d3.extent(data, function (d) { return d[xColumn]; }))
                 .nice();
 
             // normally would set the y-range to [height, 0] but by swapping it I can flip the axis and thus
             // have -log10 scale without having to do extra parsing
             yScale.range([0, innerHeight])
-                .domain(d3.extent(data, function(d) { return d[yColumn]; }))
+                .domain(d3.extent(data, function (d) { return d[yColumn]; }))
                 .nice(); // adds "padding" so the domain extent is exactly the min and max values
 
-/*
-            var zoom = d3.zoom()
-                .scaleExtent([1, 20])
-                .translateExtent([[0, 0], [width, height]])
-                .on('zoom', zoomFunction);
-*/
+            /*
+                        var zoom = d3.zoom()
+                            .scaleExtent([1, 20])
+                            .translateExtent([[0, 0], [width, height]])
+                            .on('zoom', zoomFunction);
+            */
             // append the svg object to the selection
             var svg = d3.select(this).append('svg')
                 .attr('height', height)
                 .attr('width', width)
-              .append('g')
+                .append('g')
                 .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-//                .call(zoom);
+            //                .call(zoom);
 
             // position the reset button and attach reset function
             d3.select('#resetBtn')
                 .style('top', margin.top * 1.5 + 'px')
                 .style('left', margin.left * 1.25 + 'px')
-                .on('click', reset);
+                .on('click', resetZoom);
 
             svg.append('defs').append('clipPath')
                 .attr('id', 'clip')
-              .append('rect')
+                .append('rect')
                 .attr('height', innerHeight)
                 .attr('width', innerWidth);
 
+            var curPlotTitle = svg.append("text")
+                .attr('transform', 'translate(' + width / 2 + ',' + (margin.top - 25) + ')')
+                //.attr("x", (width / 2))             
+                //.attr("y", 0 - (margin.top / 2))
+                .attr("text-anchor", "middle")  
+                .style("font-size", "16px") 
+                .style("text-decoration", "underline")  
+                .text(plotTitle);
             // add the axes
             var xAxis = d3.axisBottom(xScale);
             var yAxis = d3.axisLeft(yScale)
@@ -75,7 +84,7 @@ function volcanoPlot() {
                 .attr('transform', 'translate(0,' + innerHeight + ')')
                 .call(xAxis);
 
-            gX.append('text')
+            var gxLabel = gX.append('text')
                 .attr('class', 'label')
                 .attr('transform', 'translate(' + width / 2 + ',' + (margin.bottom - 6) + ')')
                 .attr('text-anchor', 'middle')
@@ -85,7 +94,7 @@ function volcanoPlot() {
                 .attr('class', 'y axis')
                 .call(yAxis);
 
-            gY.append('text')
+            var gyLabel = gY.append('text')
                 .attr('class', 'label')
                 .attr('transform', 'translate(' + (0 - margin.left / 1.25) + ',' + (height / 2) + ') rotate(-90)')
                 .style('text-anchor', 'middle')
@@ -101,43 +110,43 @@ function volcanoPlot() {
             var circles = svg.append('g')
                 .attr('class', 'circlesContainer');
 
-/*            circles.selectAll(".dot")
-                .data(data)
-              .enter().append('circle')
-                .attr('r', 3)
-                .attr('cx', function(d) { return xScale(d[xColumn]); })
-                .attr('cy', function(d) { return yScale(d[yColumn]); })
-                .attr('class', circleClass)
-                .on('mouseenter', tipEnter)
-                .on("mousemove", tipMove)
-                .on('mouseleave', function(d) {
-                   return tooltip.style('visibility', 'hidden');
-                });
-*/
+            /*            circles.selectAll(".dot")
+                            .data(data)
+                          .enter().append('circle')
+                            .attr('r', 3)
+                            .attr('cx', function(d) { return xScale(d[xColumn]); })
+                            .attr('cy', function(d) { return yScale(d[yColumn]); })
+                            .attr('class', circleClass)
+                            .on('mouseenter', tipEnter)
+                            .on("mousemove", tipMove)
+                            .on('mouseleave', function(d) {
+                               return tooltip.style('visibility', 'hidden');
+                            });
+            */
             var thresholdLines = svg.append('g')
                 .attr('class', 'thresholdLines');
 
             // add horizontal line at significance threshold
-/*            thresholdLines.append("svg:line")
-                .attr('class', 'threshold')
-                .attr("x1", 0)
-                .attr("x2", innerWidth)
-                .attr("y1", yScale(significanceThreshold))
-                .attr("y2", yScale(significanceThreshold));
-
-            // add vertical line(s) at fold-change threshold (and negative fold-change)
-            [foldChangeThreshold, -1 * foldChangeThreshold].forEach(function(threshold) {
-                thresholdLines.append("svg:line")
-                    .attr('class', 'threshold')
-                    .attr("x1", xScale(threshold))
-                    .attr("x2", xScale(threshold))
-                    .attr("y1", 0)
-                    .attr("y2", innerHeight);
-            });
-*/
+            /*            thresholdLines.append("svg:line")
+                            .attr('class', 'threshold')
+                            .attr("x1", 0)
+                            .attr("x2", innerWidth)
+                            .attr("y1", yScale(significanceThreshold))
+                            .attr("y2", yScale(significanceThreshold));
+            
+                        // add vertical line(s) at fold-change threshold (and negative fold-change)
+                        [foldChangeThreshold, -1 * foldChangeThreshold].forEach(function(threshold) {
+                            thresholdLines.append("svg:line")
+                                .attr('class', 'threshold')
+                                .attr("x1", xScale(threshold))
+                                .attr("x2", xScale(threshold))
+                                .attr("y1", 0)
+                                .attr("y2", innerHeight);
+                        });
+            */
 
             d3.select('#apply')
-                .on('click', applyThresholds);
+                .on('click', updatePlot);
 
             var tooltip = d3.select("body")
                 .append("div")
@@ -184,155 +193,191 @@ function volcanoPlot() {
                 else return 'dot';
             }
 
-            function reset() {
+            function resetZoom() {
                 var ease = d3.easePolyIn.exponent(4.0);
                 svg.transition().duration(750)
                     .ease(ease)
                     .call(zoom.transform, d3.zoomIdentity);
             }
 
-            function applyThresholds() {
+            function updatePlot() {
 
-/*                var ease = d3.easePolyIn.exponent(4.0);
-                svg.transition().duration(750)
-                    .ease(ease)
-                    .call(zoom.transform, d3.zoomIdentity);
-*/
-            thresholdLines.remove();
+                /* 
+                    var ease = d3.easePolyIn.exponent(4.0);
+                    svg.transition().duration(750)
+                        .ease(ease)
+                        .call(zoom.transform, d3.zoomIdentity);
+                */
 
-            foldChangeThreshold = parseFloat(d3.select("#foldchange").property("value"));
-            significanceThreshold = parseFloat(d3.select("#fdrthreshold").property("value"));
+                // get values from form 
+                foldChangeThreshold = parseFloat(d3.select("#foldchange").property("value"));
+                significanceThreshold = parseFloat(d3.select("#fdrthreshold").property("value"));
+                plotTitle = d3.select('#plottitle').property("value");
+                xAxisLabel = d3.select("#plotxaxis").property("value");
+                yAxisLabel = d3.select("#plotyaxis").property("value");
 
+                // reset the plot
+                thresholdLines.remove();
+                circles.remove();
+                curPlotTitle.remove();
+                gxLabel.remove();
+                gyLabel.remove();
+                // redraw the plot
 
-            circles.remove();
-            circles = svg.append('g')
-                .attr('class', 'circlesContainer');
+                curPlotTitle = svg.append("text")
+                    .attr('transform', 'translate(' + width / 2 + ',' + (margin.top - 25) + ')')
+                    .attr("text-anchor", "middle")  
+                    .style("font-size", "16px") 
+                    .style("text-decoration", "underline")  
+                    .text(plotTitle);
 
-            circles.selectAll(".dot")
-                .data(data)
-              .enter().append('circle')
-                .attr('r', 3)
-                .attr('cx', function(d) { return xScale(d[xColumn]); })
-                .attr('cy', function(d) { return yScale(d[yColumn]); })
-                .attr('class', circleClass)
-                .on('mouseenter', tipEnter)
-                .on("mousemove", tipMove)
-                .on('mouseleave', function(d) {
-                   return tooltip.style('visibility', 'hidden');
-                });
-            thresholdLines = svg.append('g')
-                .attr('class', 'thresholdLines');
-            // add horizontal line at significance threshold
-            thresholdLines.append("svg:line")
-                .attr('class', 'threshold')
-                .attr("x1", 0)
-                .attr("x2", innerWidth)
-                .attr("y1", yScale(significanceThreshold))
-                .attr("y2", yScale(significanceThreshold));
+                gxLabel = gX.append('text')
+                    .attr('class', 'label')
+                    .attr('transform', 'translate(' + width / 2 + ',' + (margin.bottom - 6) + ')')
+                    .attr('text-anchor', 'middle')
+                    .html(xAxisLabel || xColumn);
+    
+                gyLabel = gY.append('text')
+                    .attr('class', 'label')
+                    .attr('transform', 'translate(' + (0 - margin.left / 1.25) + ',' + (height / 2) + ') rotate(-90)')
+                    .style('text-anchor', 'middle')
+                    .html(yAxisLabel || yColumn);
 
-            // add vertical line(s) at fold-change threshold (and negative fold-change)
-            [foldChangeThreshold, -1 * foldChangeThreshold].forEach(function(threshold) {
+                circles = svg.append('g')
+                    .attr('class', 'circlesContainer');
+
+                circles.selectAll(".dot")
+                    .data(data)
+                    .enter().append('circle')
+                    .attr('r', 3)
+                    .attr('cx', function (d) { return xScale(d[xColumn]); })
+                    .attr('cy', function (d) { return yScale(d[yColumn]); })
+                    .attr('class', circleClass)
+                    .on('mouseenter', tipEnter)
+                    .on("mousemove", tipMove)
+                    .on('mouseleave', function (d) {
+                        return tooltip.style('visibility', 'hidden');
+                    });
+
+                thresholdLines = svg.append('g')
+                    .attr('class', 'thresholdLines');
+                // add horizontal line at significance threshold
                 thresholdLines.append("svg:line")
                     .attr('class', 'threshold')
-                    .attr("x1", xScale(threshold))
-                    .attr("x2", xScale(threshold))
-                    .attr("y1", 0)
-                    .attr("y2", innerHeight);
-            });
+                    .attr("x1", 0)
+                    .attr("x2", innerWidth)
+                    .attr("y1", yScale(significanceThreshold))
+                    .attr("y2", yScale(significanceThreshold));
 
-            } //applyThresholds
+                // add vertical line(s) at fold-change threshold (and negative fold-change)
+                [foldChangeThreshold, -1 * foldChangeThreshold].forEach(function (threshold) {
+                    thresholdLines.append("svg:line")
+                        .attr('class', 'threshold')
+                        .attr("x1", xScale(threshold))
+                        .attr("x2", xScale(threshold))
+                        .attr("y1", 0)
+                        .attr("y2", innerHeight);
+                });
 
-            applyThresholds();
+            } 
+
+            updatePlot();
         });
     }
 
-    chart.width = function(value) {
+    chart.width = function (value) {
         if (!arguments.length) return width;
         width = value;
         return chart;
     };
 
-    chart.height = function(value) {
+    chart.height = function (value) {
         if (!arguments.length) return height;
         height = value;
         return chart;
     };
 
-    chart.margin = function(value) {
+    chart.margin = function (value) {
         if (!arguments.length) return margin;
         margin = value;
         return chart;
     };
 
-    chart.xColumn = function(value) {
+    chart.xColumn = function (value) {
         if (!arguments.length) return xColumn;
         xColumn = value;
         return chart;
     };
 
-    chart.yColumn = function(value) {
+    chart.yColumn = function (value) {
         if (!arguments.length) return yColumn;
         yColumn = value;
         return chart;
     };
-    
-    chart.xAxisLabel = function(value) {
+
+    chart.xAxisLabel = function (value) {
         if (!arguments.length) return xAxisLabel;
         xAxisLabel = value;
         return chart;
     };
 
-    chart.yAxisLabel = function(value) {
+    chart.yAxisLabel = function (value) {
         if (!arguments.length) return yAxisLabel;
         yAxisLabel = value;
         return chart;
     };
 
-    chart.xAxisLabelOffset = function(value) {
+    chart.xAxisLabelOffset = function (value) {
         if (!arguments.length) return xAxisLabelOffset;
         xAxisLabelOffset = value;
         return chart;
     };
 
-    chart.yAxisLabelOffset = function(value) {
+    chart.yAxisLabelOffset = function (value) {
         if (!arguments.length) return yAxisLabelOffset;
         yAxisLabelOffset = value;
         return chart;
     };
 
-    chart.xTicks = function(value) {
+    chart.xTicks = function (value) {
         if (!arguments.length) return xTicks;
         xTicks = value;
         return chart;
     };
 
-    chart.yTicks = function(value) {
+    chart.yTicks = function (value) {
         if (!arguments.length) return yTicks;
         yTicks = value;
         return chart;
     };
 
-    chart.significanceThreshold = function(value) {
+    chart.significanceThreshold = function (value) {
         if (!arguments.length) return significanceThreshold;
         significanceThreshold = value;
         return chart;
     };
 
-    chart.foldChangeThreshold = function(value) {
+    chart.foldChangeThreshold = function (value) {
         if (!arguments.length) return foldChangeThreshold;
         foldChangeThreshold = value;
         return chart;
     };
 
-    chart.colorRange = function(value) {
+    chart.colorRange = function (value) {
         if (!arguments.length) return colorRange;
         colorRange = value;
         return chart;
     };
 
-    chart.sampleID = function(value) {
+    chart.sampleID = function (value) {
         if (!arguments.length) return sampleID;
         sampleID = value;
+        return chart;
+    };
+    
+    chart.plotTitle = function (value) {
+        if (!arguments.length) return plotTitle;
+        plotTitle = value;
         return chart;
     };
 
