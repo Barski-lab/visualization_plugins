aeb44cf89983f447eb387cc278855c860a402508
smcrowley8 <smcrowley8@gmail.com>
2022-10-26 18:41:00 -0400

    removed a majority of custom css. styles for graph applied directly to d3 elements instead (to make image downloaded maintain styling)

--- volcano_plot/html_data/index.js
+++ volcano_plot/html_data/index.js
@@ -50,6 +50,7 @@ function volcanoPlot() {
                 .attr('id', 'svg')
                 .attr('height', height)
                 .attr('width', width)
+                .style('color', 'white')
                 .append('g')
                 .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
             //                .call(zoom);
@@ -86,7 +87,10 @@ function volcanoPlot() {
                 .call(xAxis);
 
             var gxLabel = gX.append('text')
-                .attr('class', 'label')
+                //.attr('class', 'label')
+                .style('fill', '#666')
+                .style('font-weight', '700')
+                .style('font-size', '12px')
                 .attr('transform', 'translate(' + width / 2 + ',' + (margin.bottom - 6) + ')')
                 .attr('text-anchor', 'middle')
                 .html(xAxisLabel || xColumn);
@@ -96,17 +100,20 @@ function volcanoPlot() {
                 .call(yAxis);
 
             var gyLabel = gY.append('text')
-                .attr('class', 'label')
-                .attr('transform', 'translate(' + (0 - margin.left / 1.25) + ',' + (height / 2) + ') rotate(-90)')
+                //.attr('class', 'label')
+                .style('fill', '#666')
+                .style('font-weight', '700')
+                .style('font-size', '12px')
+                .attr('transform', 'translate(' + (0- margin.left / 1.25) + ',' + (height / 2) + ') rotate(-90)')
                 .style('text-anchor', 'middle')
                 .html(yAxisLabel || yColumn);
 
             // this rect acts as a layer so that zooming works anywhere in the svg. otherwise, if zoom is called on
             // just svg, zoom functionality will only work when the pointer is over a circle.
-            var zoomBox = svg.append('rect')
-                .attr('class', 'zoom')
-                .attr('height', innerHeight)
-                .attr('width', innerWidth);
+            // var zoomBox = svg.append('rect')
+            //     .attr('class', 'zoom')
+            //     .attr('height', innerHeight)
+            //     .attr('width', innerWidth);
 
             var circles = svg.append('g')
                 .attr('class', 'circlesContainer');
@@ -153,9 +160,9 @@ function volcanoPlot() {
             d3.select('#savesvg')
                 .on('click', exportSVG);
 
-            d3.select("#generate").on("click", writeDownloadLink);
+            d3.select("#generate").on("click", writeDownloadLinkV1);
         
-            function writeDownloadLink(){
+            function writeDownloadLinkV1(){
                 try {
                     var isFileSaverSupported = !!new Blob();
                     console.log(`is FileSaver Supported: ${isFileSaverSupported}`)
@@ -179,7 +186,7 @@ function volcanoPlot() {
                     .attr("xmlns", "http://www.w3.org/2000/svg")
                     .node().outerHTML; // .parentNode.innerHTML;
                 
-                console.log('html: ', html);
+                if(!html){console.log('issue getting html: ', html);}
                 /* 
                             d3.select("svg")
                     .attr("title", "test2")
@@ -187,10 +194,11 @@ function volcanoPlot() {
                     .attr("xmlns", "http://www.w3.org/2000/svg")
                     .node().outerHtml; */
             
-                console.log('blob: ', blob);
                 var blob = new Blob([html], {type: "image/svg+xml"}); 
+                console.log('blob: ', blob);
                 saveAs(blob, "exampleSVG.svg");
-            };
+            };            
+
             var tooltip = d3.select("body")
                 .append("div")
                 .attr('class', 'tooltip');
@@ -199,9 +207,11 @@ function volcanoPlot() {
                 tooltip.style('visibility', 'visible')
                     .style('font-size', '11px')
                     .html(
+                        '<p>' +
                         '<strong>' + sampleID + '</strong>: ' + d[sampleID] + '<br/>' +
                         '<strong>' + xColumn + '</strong>: ' + d3.format('.2f')(d[xColumn]) + '<br/>' +
-                        '<strong>' + yColumn + '</strong>: ' + d[yColumn]
+                        '<strong>' + yColumn + '</strong>: ' + d[yColumn] +
+                        '</p>'
                     );
             }
 
@@ -234,6 +244,12 @@ function volcanoPlot() {
                 else if (d[yColumn] <= significanceThreshold) return 'dot sig';
                 else if (Math.abs(d[xColumn]) >= foldChangeThreshold) return 'dot fold';
                 else return 'dot';
+            }
+            function circleStyle(d) {
+                if (d[yColumn] <= significanceThreshold && Math.abs(d[xColumn]) >= foldChangeThreshold) return 'green';
+                else if (d[yColumn] <= significanceThreshold) return 'red';
+                else if (Math.abs(d[xColumn]) >= foldChangeThreshold) return 'grey';
+                else return 'black';
             }
 
             function resetZoom() {
@@ -275,13 +291,19 @@ function volcanoPlot() {
                     .text(plotTitle);
 
                 gxLabel = gX.append('text')
-                    .attr('class', 'label')
+                    //.attr('class', 'label')
+                    .style('fill', '#666')
+                    .style('font-weight', '700')
+                    .style('font-size', '12px')
                     .attr('transform', 'translate(' + width / 2 + ',' + (margin.bottom - 6) + ')')
                     .attr('text-anchor', 'middle')
                     .html(xAxisLabel || xColumn);
     
                 gyLabel = gY.append('text')
-                    .attr('class', 'label')
+                    //.attr('class', 'label')
+                    .style('fill', '#666')
+                    .style('font-weight', '700')
+                    .style('font-size', '12px')
                     .attr('transform', 'translate(' + (0 - margin.left / 1.25) + ',' + (height / 2) + ') rotate(-90)')
                     .style('text-anchor', 'middle')
                     .html(yAxisLabel || yColumn);
@@ -295,7 +317,8 @@ function volcanoPlot() {
                     .attr('r', 3)
                     .attr('cx', function (d) { return xScale(d[xColumn]); })
                     .attr('cy', function (d) { return yScale(d[yColumn]); })
-                    .attr('class', circleClass)
+                    //.attr('class', circleClass)
+                    .style('fill', circleStyle)
                     .on('mouseenter', tipEnter)
                     .on("mousemove", tipMove)
                     .on('mouseleave', function (d) {
@@ -306,7 +329,10 @@ function volcanoPlot() {
                     .attr('class', 'thresholdLines');
                 // add horizontal line at significance threshold
                 thresholdLines.append("svg:line")
-                    .attr('class', 'threshold')
+                    //.attr('class', 'threshold') // styles instead of class
+                    .style('stroke', '#333')
+                    .style('stroke-dasharray', '5px 10px')
+                    .style('stroke-opacity', '0.35')
                     .attr("x1", 0)
                     .attr("x2", innerWidth)
                     .attr("y1", yScale(significanceThreshold))
@@ -315,7 +341,10 @@ function volcanoPlot() {
                 // add vertical line(s) at fold-change threshold (and negative fold-change)
                 [foldChangeThreshold, -1 * foldChangeThreshold].forEach(function (threshold) {
                     thresholdLines.append("svg:line")
-                        .attr('class', 'threshold')
+                        //.attr('class', 'threshold') // styles instead of class
+                        .style('stroke', '#333')
+                        .style('stroke-dasharray', '5px 10px')
+                        .style('stroke-opacity', '0.35')
                         .attr("x1", xScale(threshold))
                         .attr("x2", xScale(threshold))
                         .attr("y1", 0)
@@ -336,30 +365,101 @@ function volcanoPlot() {
                 source = source.replace(/ns\d+:href/g, 'xlink:href'); // Safari NS namespace fix.
 
 
-                console.log('3')
                 if (!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)) {
-                    console.log('4')
+                    console.log('matched xmlns tag to format')
                     source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
                 }
                 if (!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)) {
-                    console.log('5')
+                    console.log('matched svg tag to format')
                     source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
                 }
 
 
                 var preface = '<?xml version="1.0" standalone="no"?>\r\n';
                 var svgBlob = new Blob([preface, source], { type: "image/svg+xml;charset=utf-8" });
+                console.log('blob: ', svgBlob);
                 var svgUrl = URL.createObjectURL(svgBlob);
                 var downloadLink = document.createElement("a");
-                console.log('6')
                 downloadLink.href = svgUrl;
-                //downloadLink.download = name;
+                downloadLink.download = "exampleSvg.svg";
                 document.body.appendChild(downloadLink);
                 //downloadLink.setAttribute('onClick', window.open(this.href,'popUpWindow'));//onclick = "window.open(this.href,'popUpWindow')"
                 downloadLink.click();
                 document.body.removeChild(downloadLink);
             }
 
+            function downloadSvg(){
+                var svgNode = d3.select("#svg")
+                    .attr("title", "test2")
+                    .attr("version", 1.1)
+                    .attr("xmlns", "http://www.w3.org/2000/svg")
+                    .node()//.outerHTML; // .parentNode.innerHTML;
+                
+                if(!html){console.log('issue getting html: ', html);}
+
+                var cssStyleText = getCSSStyles( svgNode );
+            	appendCSS( cssStyleText, svgNode );
+            }
+
+            function getCSSStyles( parentElement ) {
+                var selectorTextArr = [];
+        
+                // Add Parent element Id and Classes to the list
+                selectorTextArr.push( '#'+parentElement.id );
+                for (var c = 0; c < parentElement.classList.length; c++)
+                        if ( !contains('.'+parentElement.classList[c], selectorTextArr) )
+                            selectorTextArr.push( '.'+parentElement.classList[c] );
+        
+                // Add Children element Ids and Classes to the list
+                var nodes = parentElement.getElementsByTagName("*");
+                for (var i = 0; i < nodes.length; i++) {
+                    var id = nodes[i].id;
+                    if ( !contains('#'+id, selectorTextArr) )
+                        selectorTextArr.push( '#'+id );
+        
+                    var classes = nodes[i].classList;
+                    for (var c = 0; c < classes.length; c++)
+                        if ( !contains('.'+classes[c], selectorTextArr) )
+                            selectorTextArr.push( '.'+classes[c] );
+                }
+        
+                // Extract CSS Rules
+                var extractedCSSText = "";
+                for (var i = 0; i < document.styleSheets.length; i++) {
+                    var s = document.styleSheets[i];
+                    
+                    try {
+                        if(!s.cssRules) continue;
+                    } catch( e ) {
+                            if(e.name !== 'SecurityError') throw e; // for Firefox
+                            continue;
+                        }
+        
+                    var cssRules = s.cssRules;
+                    for (var r = 0; r < cssRules.length; r++) {
+                        if ( contains( cssRules[r].selectorText, selectorTextArr ) )
+                            extractedCSSText += cssRules[r].cssText;
+                    }
+                }
+                
+        
+                return extractedCSSText;
+        
+                function contains(str,arr) {
+                    return arr.indexOf( str ) === -1 ? false : true;
+                }
+        
+            }
+        
+            function appendCSS( cssText, element ) {
+                var styleElement = document.createElement("style");
+                styleElement.setAttribute("type","text/css"); 
+                styleElement.innerHTML = cssText;
+                var refNode = element.hasChildNodes() ? element.children[0] : null;
+                element.insertBefore( styleElement, refNode );
+            }
+        
+        
 
             updatePlot();
         });
